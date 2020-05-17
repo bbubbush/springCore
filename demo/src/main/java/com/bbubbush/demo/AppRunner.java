e88ce444c6cb6ad79135dced1546a6846385e0c3
bbubbush <bbubbush@gmail.com>
2020-05-17 20:06:44 +0900

    다국어 지원 프로퍼티 빈 추가

--- demo/src/main/java/com/bbubbush/demo/AppRunner.java
+++ demo/src/main/java/com/bbubbush/demo/AppRunner.java
@@ -6,23 +6,40 @@ import com.bbubbush.demo.logic.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.MessageSource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
     DemoController controller;
-
     @Autowired
     DemoService service;
-
     @Autowired
     DemoRepository repository;
+    @Autowired
+    Environment environment;
+    @Autowired
+    MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println(controller);
         System.out.println(service);
         System.out.println(repository);
+        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(Arrays.toString(environment.getActiveProfiles()));
+        System.out.println(environment.getProperty("hello"));
+        System.out.println(environment.getProperty("enviroment.scope"));
+
+        while(true) {
+            System.out.println(messageSource.getMessage("greeting", new String[]{"bbubbush"}, Locale.getDefault()));
+            System.out.println(messageSource.getMessage("greeting", new String[]{"bbubbush"}, Locale.ENGLISH));
+            Thread.sleep(3000);
+        }
     }
 }
--- demo/src/main/java/com/bbubbush/demo/DemoApplication.java
+++ demo/src/main/java/com/bbubbush/demo/DemoApplication.java
@@ -2,12 +2,26 @@ package com.bbubbush.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource () {
+        ReloadableResourceBundleMessageSource reloadableResourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+        reloadableResourceBundleMessageSource.setBasename("classpath:messages");
+        reloadableResourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        reloadableResourceBundleMessageSource.setCacheSeconds(3);
+        return reloadableResourceBundleMessageSource;
     }
 
 }
